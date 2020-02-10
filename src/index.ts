eca1c5c460bb1963b060142c524f1ea0be746505
Kathrin Ayer <kathrin.ayer@gmail.com>
2020-02-09 21:03:22 -0500

    fix async problem

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,53 @@
-const callAPIforData = () => {
-  fetch('http://localhost:3000/dogs?type=hound', {
-    method: 'GET',
-    headers: new Headers(
-      {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-    ),
-  })
-  .then(data => data.json())
-  .then(data => {
-    console.log(data);
-  });
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const API_BASE_URL = 'http://localhost:3000/';
+
+const constructQuery = (base: string, query: object) => {
+  const keyMap = Object.keys(query);
+  return keyMap.reduce((acc: string, key: string, index: number)=> {
+    acc += `${key}=${query[key]}`;
+
+    if (index < keyMap.length -1){
+      acc += '&';
+    }
+    
+    return acc;
+  }, `${base}?`);
 };
 
-callAPIforData();
+const callAPIforData = async (query?: object) => {
+  return new Promise((resolve, reject)=> {
+    let url = API_BASE_URL;
+
+    if (query) {
+      url = constructQuery(API_BASE_URL, query);
+    }
+  
+    fetch(url, {
+      method: 'GET',
+      headers: new Headers(
+        {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      ),
+    })
+    .then(data => data.json())
+    .then(data => {
+      resolve(data);
+    })
+    .catch((err)=> {
+      reject(err);
+    });
+  });
+  
+};
+
+const initPage = async () => {
+  const dropDownItems = await callAPIforData();
+  console.log(dropDownItems);
+  callAPIforData({type: 'hound'});
+};
+
+initPage();
