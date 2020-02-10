1cedc4ff10af114e3c4ab5e41972a5bacb6b84d8
Kathrin Ayer <kathrin.ayer@gmail.com>
2020-02-10 08:32:58 -0500

    style navigation

--- src/dogController/index.ts
+++ src/dogController/index.ts
@@ -26,44 +26,60 @@ class DogController implements DogController {
     this.dogModal.innerHTML = '';
   };
 
-  paginationButton = (value: number, title: string) => {
+  private paginationButton = (value: number, title: string) => {
     const button = document.createElement('button');
+    button.classList.add('col-thirds');
     button.setAttribute('value', `${value}`);
     button.innerHTML = title;
     button.onclick = this.getPage;
     return button;
   };
 
-  getPage = async (event: Event) => {
+  private getPage = async (event: MouseEvent) => {
     const button: HTMLButtonElement = event.target as HTMLButtonElement;
     const response = await DogModel.callAPIforData({page: parseInt(button.value)});
     this.dogThumbnails(response);
   };
+
+  dogNavigation = (pageItemsCount: number, nextPage?: number, prevPage?: number) => {
+    const navigation = document.createElement('section');
+    navigation.classList.add('dog-navigation', 'flex-container');
+
+    const label = document.createElement('div');
+    label.classList.add('col-thirds');
+    label.innerHTML = `number of dogs ${pageItemsCount}`;
+    navigation.append(label);
+
+    if (prevPage !== undefined) {
+      const button = this.paginationButton(prevPage, 'previous page');
+      navigation.prepend(button);
+    }
+
+    if (nextPage !== undefined) {
+      const button = this.paginationButton(nextPage, 'next page');
+      navigation.append(button);
+    }
+
+    return navigation;
+  }
   
   dogThumbnails = (response: any) => {
     const { pageItems, prevPage, nextPage} = response.data;
   
     this.dogContainer.innerHTML = '';
-    this.dogContainer.innerHTML = `<section class="dog-thumbnails">${pageItems.reduce((acc: string, url: string) => {
+    this.dogContainer.innerHTML = `<section class="dog-thumbnails flex-container">${pageItems.reduce((acc: string, url: string) => {
       acc += `<div class="col"><img class="dog-trigger" src="${url}" alt="dog"/></div>`;
       return acc;
     }, '')}</section>`;
   
     const allImages = this.dogContainer.getElementsByClassName('dog-trigger');
+
     Array.from(allImages)
       .forEach((element: HTMLImageElement )=> {
         element.addEventListener('click', this.modalOpen);
       });
   
-    if (nextPage !== undefined) {
-      const button = this.paginationButton(nextPage, 'next page');
-      this.dogContainer.prepend(button);
-    }
-  
-    if (prevPage !== undefined) {
-      const button = this.paginationButton(prevPage, 'previous page');
-      this.dogContainer.prepend(button);
-    }
+    this.dogContainer.prepend(this.dogNavigation(allImages.length, nextPage, prevPage));
   };
 }
 
